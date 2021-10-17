73ceb2af7a4205a310812dbd13e1f96341dec334
JustAndrej2510 <andrey.pisaryk@gmail.com>
2021-10-18 00:38:19 +0300

    change .gitignore and add .sfdx folder

---
+++ .sfdx/typings/lwc/sobjects/Sensor__c.d.ts
@@ -0,0 +1,72 @@
+declare module "@salesforce/schema/Sensor__c.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Sensor__c.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Sensor__c.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Sensor__c.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/Sensor__c.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Sensor__c.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Sensor__c.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Sensor__c.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Sensor__c.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Sensor__c.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Sensor__c.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Sensor__c.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
+declare module "@salesforce/schema/Sensor__c.LastViewedDate" {
+  const LastViewedDate:any;
+  export default LastViewedDate;
+}
+declare module "@salesforce/schema/Sensor__c.LastReferencedDate" {
+  const LastReferencedDate:any;
+  export default LastReferencedDate;
+}
+declare module "@salesforce/schema/Sensor__c.Status__c" {
+  const Status__c:string;
+  export default Status__c;
+}
+declare module "@salesforce/schema/Sensor__c.Sensor_model__c" {
+  const Sensor_model__c:string;
+  export default Sensor_model__c;
+}
+declare module "@salesforce/schema/Sensor__c.Base_Station__r" {
+  const Base_Station__r:any;
+  export default Base_Station__r;
+}
+declare module "@salesforce/schema/Sensor__c.Base_Station__c" {
+  const Base_Station__c:any;
+  export default Base_Station__c;
+}
